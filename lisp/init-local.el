696183ebba329df0abdccc2bb4f65151f20f2cf0
Niklas Rehfeld <nikr@canva.com>
2023-06-12 15:44:10 +1200

    Add init-local.el for local changes

---
+++ lisp/init-local.el
@@ -0,0 +1,23 @@
+;;; init-local.el --- Local additions
+
+
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+;;; Treemacs
+(when (maybe-require-package 'treemacs)
+  (require-package 'treemacs-projectile)
+  (setq-default treemacs-display-current-project-exclusively t)
+  (setq-default treemacs-project-follow-mode)
+  (treemacs-resize-icons 12)
+  )
+
+(with-eval-after-load 'ag 
+  (add-to-list 'ag-ignore-list "projectile.cache"))
+
+(provide 'init-local)
+
+;;; init-local.el ends here
