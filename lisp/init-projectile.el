4cc6540c31e176f392b784aeda72b289c967e5b6
Niklas Rehfeld <nikr@canva.com>
2023-06-12 15:45:23 +1200

    some more various changes.

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -7,14 +7,23 @@
 
   ;; Shorter modeline
   (setq-default projectile-mode-line-prefix " Proj")
-
+  (setq projectile-enable-caching t)
   (when (executable-find "rg")
     (setq-default projectile-generic-command "rg --files --hidden -0"))
 
   (with-eval-after-load 'projectile
     (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
-  (maybe-require-package 'ibuffer-projectile))
+  (maybe-require-package 'ibuffer-projectile)
+  (defun projectile-vc-root-dir (dir)
+    "Retrieve the root directory of the project at DIR using `vc-root-dir'."
+    (let ((default-directory dir))
+      (vc-root-dir)))
+
+  (setq projectile-project-root-functions '(projectile-vc-root-dir
+                                            projectile-root-local
+                                            projectile-root-marked
+                                            projectile-root-bottom-up)))
 
 
 (provide 'init-projectile)
