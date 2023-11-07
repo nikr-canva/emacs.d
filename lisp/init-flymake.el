88660640275fc712eed7fd0dbc1a517931840e37
Niklas Rehfeld <nikr@canva.com>
2023-11-07 13:49:35 +1300

    remove flymake-shellcheck as it's now builtin

--- lisp/init-flymake.el
+++ lisp/init-flymake.el
@@ -31,9 +31,5 @@
             (lambda ()
               (add-hook 'eldoc-documentation-functions 'flymake-eldoc-function nil t))))
 
-(when  (maybe-require-package 'flymake-shellcheck)
-  (add-hook 'sh-mode-hook 'flymake-shellcheck-load)
-  (setq-default 'flymake-shellcheck-allow-external-files t))
-
 (provide 'init-flymake)
 ;;; init-flymake.el ends here
