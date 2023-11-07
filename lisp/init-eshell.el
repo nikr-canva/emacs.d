7b87cbf41f31009dea175763167ee2f47b3c6da1
Niklas Rehfeld <nikr@canva.com>
2023-11-07 13:45:48 +1300

    remove eshell-p10k altogether.

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -2,13 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (maybe-require-package 'eshell-p10k)
-
-  (with-eval-after-load 'eshell
-    (setq eshell-prompt-function #'eshell-p10k-prompt-function
-          eshell-prompt-regexp eshell-p10k-prompt-string))
-  )
-
 (provide 'init-eshell)
 
 ;;; init-eshell.el ends here
