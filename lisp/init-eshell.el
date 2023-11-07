f7c3132daaf06c2cb1af1cb9833c38e5b7c6cf39
Niklas Rehfeld <nikr@canva.com>
2023-11-07 13:40:05 +1300

    make eshell-p10k optional

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -2,12 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'eshell-p10k)
+(when (maybe-require-package 'eshell-p10k)
 
-(with-eval-after-load 'eshell
-  (setq eshell-prompt-function #'eshell-p10k-prompt-function
-        eshell-prompt-regexp eshell-p10k-prompt-string))
-
+  (with-eval-after-load 'eshell
+    (setq eshell-prompt-function #'eshell-p10k-prompt-function
+          eshell-prompt-regexp eshell-p10k-prompt-string))
+  )
 
 (provide 'init-eshell)
 
