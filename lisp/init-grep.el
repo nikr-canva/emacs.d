5775d742550c4f019eaa1a8252af029cedfd8dd6
Niklas Rehfeld <nikr@canva.com>
2025-12-23 19:08:09 +1300

    set ag to reuse search buffer

--- lisp/init-grep.el
+++ lisp/init-grep.el
@@ -13,6 +13,12 @@
   (dolist (key (list (kbd "C-c C-q") (kbd "w")))
     (define-key grep-mode-map key 'wgrep-change-to-wgrep-mode)))
 
+(when (and (executable-find "ag")
+           (maybe-require-package 'ag))
+  (require-package 'wgrep-ag)
+  (setq-default ag-highlight-search t)
+  (setq ag-?reuse-buffers t)
+  (global-set-key (kbd "M-?") 'ag-project))
 
 (when (and (executable-find "rg")
            (maybe-require-package 'rg))
